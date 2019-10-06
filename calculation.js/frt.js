fb28a78e5cf5042d39f27bbc64bff21575adf87c
Ezege1 <davidamaogu042@gmail.com>
2019-10-05 20:02:32 -0700

    first commit

---
+++ calculation.js/frt.js
@@ -0,0 +1,35 @@
+// this is single line comment  
+/* This is a  
+Multi-line comment 
+*/
+
+//Let us start with the traditional “Hello World” example".
+// var message = "Hello World" 
+// console.log(message)
+
+// let alert = "Hi Mr David, you are awsome";
+// console.log(alert);
+
+// v = 15 
+// function f1() { 
+//    "use strict"; 
+//    var v = "Hi!  I'm a strict mode script!"; 
+// }
+// console.log(v);
+
+//Declaration using var keyword 
+//var  variable_name
+
+/*ES6 introduces the following variable declaration syntax −
+Using the let.
+Using the const.*/
+
+/*Variable initialization refers to the process of storing a value in the variable. 
+A variable may be initialized either at the time of its declaration or at a later point in time.*/
+
+//Syntax for variable Declaration and Initialization using var keyword 
+//var variable_name or identifier = value
+
+//Example : Using Variables
+var name = "David" 
+console.log("I am calld : "+name)
